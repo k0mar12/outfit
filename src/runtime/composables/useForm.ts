d434fa5ef9c923dd72c40edf885ed3b0ea99ee4d
k0mar12 <vlaldovedance@gmail.com>
2024-05-10 21:50:03 +0300

    fix: sending files

--- src/runtime/composables/useForm.ts
+++ src/runtime/composables/useForm.ts
@@ -184,11 +184,21 @@ export const useForm = (opts = {}) => {
   const makeRequest = (params, validated) => {
     let fields = validated
 
-    if (otps?.isFormData) {
+    if (opts?.isFormData) {
       const formData = new FormData()
 
       for (const [key, value] of Object.entries(fields)) {
-        formData.append(key, typeof value === 'object' ? JSON.stringify(value) : value)
+        if (value instanceof FileList) {
+          for (const file of value) {
+            formData.append(`${key}[]`, file)
+          }
+        } else if (value instanceof File) {
+          formData.append(key, value)
+        } else if (typeof value === 'object') {
+          formData.append(key, JSON.stringify(value))
+        } else {
+          formData.append(key, value)
+        }
       }
 
       fields = formData

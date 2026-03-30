e5399537867fa22b7b21f611ec87898e956909dd
k0mar12 <vlaldovedance@gmail.com>
2026-03-30 11:59:25 +0300

    feat: add data fetching compasable

---
+++ src/runtime/composables/useDataFetch.ts
@@ -0,0 +1,119 @@
+import { klona as deepClone } from 'klona/full'
+import { flatten } from 'flat'
+
+export const useDataFetch = async (name, path, options = {}) => {
+  const { $spaFetch } = useNuxtApp()
+  const { push } = useRouter()
+  const { getBindingQueryValues } = useQuery()
+  const { filters: filtersQuery, other: otherQuery } = getBindingQueryValues()
+
+  const filters = reactive({ ...deepClone(options?.filters), ...(options?.isSilent ? {} : filtersQuery) })
+  const other = reactive({ page: 1, ...(options?.isSilent ? {} : otherQuery) })
+
+  /**
+   * Get current page for component easy reading
+   *
+   * @return Number
+   */
+  const currentPage = computed({
+    get () {
+      return other.page
+    },
+    set (value) {
+      other.page = value
+    }
+  })
+
+  /**
+   * Prepare current filters for backend
+   *
+   * @return Object
+   */
+  const preparedFilters = computed(() => {
+    const inLine = flatten(filters)
+
+    return Object.keys(inLine).filter(key => inLine[key] !== null).reduce((result, key) => {
+      result[`filter[${key}]`] = inLine[key]
+
+      return result
+    }, {})
+  })
+
+  /**
+   * Merge all query
+   *
+   * @return Object
+   */
+  const validQuery = computed(() => ({ ...other, ...preparedFilters.value }))
+
+  const { data, refresh, pending } = await useAsyncData(name, () => $spaFetch(path, {
+    query: validQuery.value,
+    ...(options?.fetch ?? {})
+  }))
+
+  /**
+   * Make the request with builded query
+   *
+   */
+  const make = async () => {
+    await refresh()
+
+    if (options?.isSilent !== true) {
+      push({ query: validQuery.value })
+    }
+  }
+
+  /**
+   * Change current apge
+   *
+   * @param page
+   */
+  const changePage = async (page) => {
+    currentPage.value = page
+    await make()
+  }
+
+  /**
+   * Change items per page
+   *
+   * @param {number} count
+   */
+  const changePerPage = async (count) => {
+    other.per_page = count
+    await make()
+  }
+
+  /**
+   * Make filtering
+   *
+   */
+  const filter = async () => {
+    currentPage.value = 1
+    await make()
+  }
+
+  /**
+   * Clear filter
+   *
+   */
+  const clearFilter = async () => {
+    Object.assign(filters, deepClone(options?.filters))
+    await make()
+  }
+
+  watch(filters, async () => {
+    await filter()
+  }, { deep: true })
+
+  return {
+    pending,
+    refresh,
+    data,
+    filters,
+    currentPage,
+    changePage,
+    changePerPage,
+    filter,
+    clearFilter
+  }
+}
---
+++ src/runtime/composables/useQuery.ts
@@ -0,0 +1,34 @@
+export const useQuery = () => {
+  const { query } = useRoute()
+
+  const convertValue = (value) => {
+    if (!isNaN(value)) {
+      return parseFloat(value)
+    } else if (value === 'true' || value === 'false') {
+      return value === 'true'
+    } else if (value.includes(',')) {
+      return value.split(',').map(item => convertValue(item.trim()))
+    }
+
+    return value
+  }
+
+  const getBindingQueryValues = () => {
+    return Object.keys(query).reduce((params, key) => {
+      const match = key.match(/^filter\[(.+)\]$/)
+      const value = convertValue(query[key])
+
+      if (match) {
+        params.filters[match[1]] = value
+      } else {
+        params.other[key] = value
+      }
+
+      return params
+    }, { filters: {}, other: {} })
+  }
+
+  return {
+    getBindingQueryValues
+  }
+}
